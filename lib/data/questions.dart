daf5e4372a4d882e846cd40f05df176c236a2d8b
Idris Azim <edrisazim25@gmail.com>
2024-09-10 13:17:25 +0300

    Adding new features

---
+++ lib/data/questions.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_ogreniyorum/models/quiz_questions.dart';
+
+const questions = [
+  QuizQuestion('Who would I r*b one out if I had only one choice?', [
+    'Abdul',
+    'Za ker',
+    'Dayi',
+    'Mayna'
+  ],),
+  QuizQuestion('Favorite Don Polo album?', [
+    'Salamalekomalekomsalad',
+    'Noladiga',
+    'Hello every follow me',
+    'Wazaa'
+  ],),
+  QuizQuestion('Favorite car brand', [
+    'Toyota',
+    'Ford',
+    'Mercedes',
+    'BMW'
+  ],),
+  QuizQuestion('Which language do I speak fluently the most', [
+    'Facts',
+    'Pashayi',
+    'Pashto',
+    'Urdu'
+  ],),
+  QuizQuestion('Favorite raper', [
+    'None',
+    'Konya',
+    'Drake',
+    'Eminem'
+  ],)
+  
+];
--- lib/questions.dart
+++ lib/questions.dart
@@ -12,6 +12,31 @@ class Questions extends StatefulWidget {
 class _QuestionsState extends State<Questions> {
   @override
   Widget build(context) {
-    return Text("Questions");
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('The question...', style: TextStyle(color: Colors.white, fontSize: 30.0, decoration: TextDecoration.none)),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 2'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 3'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 4'),
+          ),
+        ],
+      ),
+    );
   }
 }
