c648491f4e72522654053c76f44189e02549b8ec
Idris Azim <edrisazim25@gmail.com>
2024-09-11 20:13:48 +0300

    Button stuff

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -8,15 +8,20 @@ class AnswerButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {},
-      style: ElevatedButton.styleFrom(
-          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-          backgroundColor: Color.fromARGB(0, 0, 0, 0),
-          foregroundColor: Colors.white,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30))),
-      child: Text(answerText),
-    );
+    return Container(
+        margin: const EdgeInsets.all(4.0),
+        child: SizedBox(
+            width: 200,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+                  backgroundColor: const Color.fromARGB(0, 0, 0, 0),
+                  foregroundColor: const Color.fromARGB(255, 251, 225, 225),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25.0))),
+              child: Text(answerText),
+            )));
   }
 }
--- lib/questions.dart
+++ lib/questions.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ogreniyorum/answer_button.dart';
-import 'package:flutter_ogreniyorum/data/questions.dart';
+import 'package:flutter_ogreniyorum/data/questions_list.dart';
 
 class Questions extends StatefulWidget {
   const Questions({super.key});
@@ -14,23 +14,34 @@ class Questions extends StatefulWidget {
 class _QuestionsState extends State<Questions> {
   @override
   Widget build(context) {
-    return Container(
-      color: const Color.fromARGB(255, 21, 176, 4),
-      child: SizedBox(
-      width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text('The question...', style: TextStyle(color: Colors.white, fontSize: 30.0, decoration: TextDecoration.none)),
-          const SizedBox(height: 30),
-          AnswerButton('Answer Text 1...', (){}),
-          AnswerButton('Answer Text 2...', (){}),
-          AnswerButton('Answer Text 3...', (){}),
-          AnswerButton('Answer Text 4...', (){}),
+    final currentQuestion = questions[0];
 
-        ],
-      ),
-    )
-    );
+    return Container(
+        color: const Color.fromARGB(255, 21, 176, 4),
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ...currentQuestion.answers.map((answer) { // The 3 dot here converts to list to individual items
+                return AnswerButton(answer, (){});
+              }),
+              Container(
+                  margin: const EdgeInsets.only(
+                      right: 30.0, left: 30.0, bottom: 10.0),
+                  child: Text(currentQuestion.text,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                        decoration: TextDecoration.none,
+                      ))),
+              const SizedBox(height: 30),
+              AnswerButton(currentQuestion.answers[0], () {}),
+              AnswerButton(currentQuestion.answers[1], () {}),
+              AnswerButton(currentQuestion.answers[2], () {}),
+              AnswerButton(currentQuestion.answers[3], () {}),
+            ],
+          ),
+        ));
   }
 }
